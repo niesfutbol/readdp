536910b0641eb63e3500331c302338d733b345fb
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:43:30 -0700

    🎨 Format with Styler

--- R/aux.R
+++ R/aux.R
@@ -1,5 +1,5 @@
 new_path_worker <- function(path_example) {
-    structure <- list()
-    class(structure) <- "path_worker"
-    return(structure)
+  structure <- list()
+  class(structure) <- "path_worker"
+  return(structure)
 }
