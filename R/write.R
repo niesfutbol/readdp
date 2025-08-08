2b44ba049d9f7e9d7c5d2af18fc910cd69d180cd
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 14:58:25 -0700

    ♻️ Utilize `path_worker` object

--- R/write.R
+++ R/write.R
@@ -1,10 +1,9 @@
 write_csv <- function(datos, file) {
   Path_Worker <- new_path_worker(file)
   directory <- Path_Worker$directory
-  name <- basename(file) |>
-    stringr::str_split("\\.")
+  name <- Path_Worker$filename
   paquete <-
     frictionless::create_package() |>
-    frictionless::add_resource(resource_name = name[[1]][1], data = datos)
+    frictionless::add_resource(resource_name = name, data = datos)
   paquete |> frictionless::write_package(directory)
 }
