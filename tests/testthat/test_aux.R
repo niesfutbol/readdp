fda275d2860c389dba813e5d2215e7136e2ab1b0
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:49:41 -0700

    ♻️✅ Refactor test: DRY

--- tests/testthat/test_aux.R
+++ tests/testthat/test_aux.R
@@ -1,12 +1,10 @@
 describe("path_worker", {
+  path_example <- "results/example.csv"
+  Path_Worker <- new_path_worker(path_example)
   it("Return directory", {
-    path_example <- "results/example.csv"
-    Path_Worker <- new_path_worker(path_example)
     expect_s3_class(Path_Worker, "path_worker")
   })
   it("Check if the path is a directory", {
-    path_example <- "results/example.csv"
-    Path_Worker <- new_path_worker(path_example)
     expected_directory <- "results"
     obtained_directory <- Path_Worker$directory
     expect_equal(obtained_directory, expected_directory)
