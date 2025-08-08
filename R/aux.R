4ca10e08b86c4de4c8a367b2ad5adb784570ab99
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:54:29 -0700

    ✅ Pass test

--- R/aux.R
+++ R/aux.R
@@ -1,5 +1,10 @@
 new_path_worker <- function(path_example) {
-  structure <- list(directory = dirname(path_example))
+  structure <- list(directory = dirname(path_example), filename = .get_filename(path_example))
   class(structure) <- "path_worker"
   return(structure)
+}
+
+.get_filename <- function(path_example) {
+  filename <- tools::file_path_sans_ext(basename(path_example))
+  return(filename)
 }
