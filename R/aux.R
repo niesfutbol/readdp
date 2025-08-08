437cec131f0bbd82218ddeb93fb06ec7075117d5
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:58:49 -0700

    ✅ Pass test

--- R/aux.R
+++ R/aux.R
@@ -1,5 +1,8 @@
 new_path_worker <- function(path_example) {
-  structure <- list(directory = dirname(path_example), filename = .get_filename(path_example))
+  structure <- list(
+    directory = dirname(path_example), filename = .get_filename(path_example),
+    datapackage_path = .get_data_package_path()
+  )
   class(structure) <- "path_worker"
   return(structure)
 }
@@ -7,4 +10,8 @@ new_path_worker <- function(path_example) {
 .get_filename <- function(path_example) {
   filename <- tools::file_path_sans_ext(basename(path_example))
   return(filename)
+}
+
+.get_data_package_path <- function() {
+  return("results/datapackages.json")
 }
