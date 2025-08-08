c634e12ac221cdcf20ef22995e780c4c8ecb5131
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:52:42 -0700

    🛑🧪 Test `new_path_worker()`: property `filename`

--- tests/testthat/test_aux.R
+++ tests/testthat/test_aux.R
@@ -1,12 +1,17 @@
 describe("path_worker", {
   path_example <- "results/example.csv"
   Path_Worker <- new_path_worker(path_example)
-  it("Return directory", {
+  it("Check class type", {
     expect_s3_class(Path_Worker, "path_worker")
   })
   it("Check if the path is a directory", {
     expected_directory <- "results"
     obtained_directory <- Path_Worker$directory
     expect_equal(obtained_directory, expected_directory)
+  })
+  it("Check filename", {
+    expected_filename <- "example"
+    obtained_filename <- Path_Worker$filename
+    expect_equal(obtained_filename, expected_filename)
   })
 })
