ff97fb28bcf2e6968c9e24c2201302b06465232b
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 15:08:14 -0700

    💄 Delete enter

--- R/write.R
+++ R/write.R
@@ -2,8 +2,7 @@ write_csv <- function(datos, file) {
   Path_Worker <- new_path_worker(file)
   directory <- Path_Worker$directory
   name <- Path_Worker$filename
-  paquete <-
-    frictionless::create_package() |>
+  paquete <- frictionless::create_package() |>
     frictionless::add_resource(resource_name = name, data = datos)
   paquete |> frictionless::write_package(directory)
 }
