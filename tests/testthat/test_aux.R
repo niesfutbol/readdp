7adf5ad3b3442aab58f23586bddf524717e84879
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 13:42:47 -0700

    🛑🧪 Test `new_path_worker()`: class type

--- tests/testthat/test_aux.R
+++ tests/testthat/test_aux.R
@@ -2,5 +2,6 @@ describe("path_worker", {
   it("Return directory", {
     path_example <- "results/example.csv"
     Path_Worker <- new_path_worker(path_example)
+    expect_s3_class(Path_Worker, "path_worker")
   })
 })
