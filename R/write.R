ec56eb78ddc4cc27046bc98a5639707e6b285935
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-09 00:57:53 -0700

    ♻️ Refactor: rename functions

--- R/write.R
+++ R/write.R
@@ -8,18 +8,18 @@ write_csv <- function(datos, file) {
 select_writer_if_exists <- function(datos, path_worker) {
   path_dp <- path_worker$datapackage_path
   if (file.exists(path_dp)) {
-    return(write_csv_with_existed_dp(datos, path_worker))
+    return(datapackage_with_existed_dp(datos, path_worker))
   }
-  return(write_csv_with_not_existed_dp(datos, path_worker))
+  return(datapackage_with_not_existed_dp(datos, path_worker))
 }
 
-write_csv_with_not_existed_dp <- function(datos, path_worker) {
+datapackage_with_not_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::create_package() |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
   return(paquete)
 }
 
-write_csv_with_existed_dp <- function(datos, path_worker) {
+datapackage_with_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::read_package(path_worker$datapackage_path) |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
   return(paquete)
