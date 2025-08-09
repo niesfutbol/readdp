aafbdf4d72abdc4bac552484ac09f732a14519ce
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 20:59:48 -0700

    ♻️ Use `path_worker`

--- R/write.R
+++ R/write.R
@@ -8,12 +8,11 @@ write_csv <- function(datos, file) {
 }
 
 write_csv_with_existed_dp <- function(datos, file) {
-  directory <- dirname(file)
-  name <- basename(file) |>
-    stringr::str_split("\\.")
-  path_dp <- glue::glue("{directory}/datapackage.json")
-  paquete <-
-  frictionless::read_package(path_dp) |>
-  frictionless::add_resource(resource_name = name[[1]][1], data = datos)
+  Path_Worker <- new_path_worker(file)
+  directory <- Path_Worker$directory
+  name <- Path_Worker$filename
+  path_dp <- Path_Worker$datapackage_path
+  paquete <- frictionless::read_package(path_dp) |>
+    frictionless::add_resource(resource_name = name, data = datos)
   paquete |> frictionless::write_package(directory)
 }
