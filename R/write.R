84e224f462725fda6ceedf48f860645f769d7cac
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-09 00:55:57 -0700

    ♻️ Refactor: return datapackage

--- R/write.R
+++ R/write.R
@@ -1,26 +1,26 @@
 #' @export
 write_csv <- function(datos, file) {
   Path_Worker <- new_path_worker(file)
-  select_writer_if_exists(datos, Path_Worker)
+  select_writer_if_exists(datos, Path_Worker) |>
+  frictionless::write_package(Path_Worker$directory)
 }
 
 select_writer_if_exists <- function(datos, path_worker) {
   path_dp <- path_worker$datapackage_path
   if (file.exists(path_dp)) {
-    write_csv_with_existed_dp(datos, path_worker)
-    return()
+    return(write_csv_with_existed_dp(datos, path_worker))
   }
-  write_csv_with_not_existed_dp(datos, path_worker)
+  return(write_csv_with_not_existed_dp(datos, path_worker))
 }
 
 write_csv_with_not_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::create_package() |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
-  paquete |> frictionless::write_package(path_worker$directory)
+  return(paquete)
 }
 
 write_csv_with_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::read_package(path_worker$datapackage_path) |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
-  paquete |> frictionless::write_package(path_worker$directory)
+  return(paquete)
 }
