e449700fc13293a02f65a6e39f9e99809136f3d3
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 21:57:33 -0700

    ♻️ Refactor: sort functions

--- R/write.R
+++ R/write.R
@@ -3,6 +3,15 @@ write_csv <- function(datos, file) {
   select_writer_if_exists(datos, Path_Worker)
 }
 
+select_writer_if_exists <- function(datos, path_worker) {
+  path_dp <- path_worker$datapackage_path
+  if (file.exists(path_dp)) {
+    write_csv_with_existed_dp(datos, path_worker)
+    return()
+  }
+  write_csv_with_not_existed_dp(datos, path_worker)
+}
+
 write_csv_with_not_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::create_package() |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
@@ -13,13 +22,4 @@ write_csv_with_existed_dp <- function(datos, path_worker) {
   paquete <- frictionless::read_package(path_worker$datapackage_path) |>
     frictionless::add_resource(resource_name = path_worker$filename, data = datos)
   paquete |> frictionless::write_package(path_worker$directory)
-}
-
-select_writer_if_exists <- function(datos, path_worker) {
-  path_dp <- path_worker$datapackage_path
-  if (file.exists(path_dp)) {
-    write_csv_with_existed_dp(datos, path_worker)
-    return()
-  }
-  write_csv_with_not_existed_dp(datos, path_worker)
 }
