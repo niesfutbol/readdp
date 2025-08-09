4829944494070a938b275728e9ee2ab51cbdfeb9
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-08 19:32:51 -0700

    ⚡️✅ Use old folder
    Antes usaba un folder consignado. Ahora usa el folder con que ya se hacía en las pruebas.

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -13,15 +13,15 @@ describe("write_csv", {
     write_csv(example_data, path_example)
     expect_true(file.exists(path_example))
     expect_true(file.exists("/workdir/borrame/datapackage.json"))
-    tear_down("/workdir/borrame")
   })
   it("writes a csv and datapackage when the folder is not empty", {
-    path_example <- "/workdir/tests/data/example.csv"
+    path_example <- "/workdir/borrame/example_2.csv"
     write_csv(example_data, path_example)
     expect_true(file.exists(path_example))
-    resource <- jsonlite::fromJSON("/workdir/tests/data/datapackage.json")$resources
+    resource <- jsonlite::fromJSON("/workdir/borrame/datapackage.json")$resources
     expected_nrows <- 2
     obtained_nrows <- nrow(resource)
     expect_equal(obtained_nrows, expected_nrows)
+    tear_down("/workdir/borrame")
   })
 })
